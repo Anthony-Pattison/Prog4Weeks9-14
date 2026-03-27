89d04dcf7eac04f224c22e34915e5170dc35ed19
valor87 <134859690+valor87@users.noreply.github.com>
2026-03-27 01:07:10 -0400

    importing text mesh pro and starting on firing a weapon

--- Assets/Scripts/Assignment6/playerWeapon.cs
+++ Assets/Scripts/Assignment6/playerWeapon.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class playerWeapon : MonoBehaviour
 {
@@ -9,23 +11,55 @@ public class playerWeapon : MonoBehaviour
     float extraAmmo = 5;
     [SerializeField]
     float weaponRange = 5;
+    [SerializeField]
+    float weaponCoolDown = 0.25f;
+    public AudioSource AudioSource;
+    public AudioClip gunShotAC;
+
+
+    bool firedWeapon = false;
+    Vector3 orgin;
+    Vector3 direction;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
         drawRange();
+        weaponInput();
     }
-
+    void weaponInput()
+    {
+        orgin = transform.position + transform.forward + (transform.up);
+        direction = transform.forward;
+        if (Input.GetMouseButtonDown(0) && !firedWeapon)
+        {
+            StartCoroutine(fireWeapon());
+        }
+    }
     void drawRange()
     {
-        Vector3 orgin = transform.position + transform.forward + (transform.up);
-        Vector3 direction = transform.forward;
-        Physics.Raycast(orgin, direction, weaponRange);
-        Debug.DrawRay(orgin, direction * weaponRange, Color.pink);
+        Debug.DrawRay(orgin, direction * weaponRange, Color.red);
+    }
+    
+    IEnumerator fireWeapon()
+    {
+        RaycastHit hit;
+        firedWeapon = true;
+        AudioSource.PlayOneShot(gunShotAC);
+        if (Physics.Raycast(orgin, direction, out hit, weaponRange))
+        {
+            Debug.Log(hit.collider.name);
+            if (hit.collider != null)
+            {
+                Debug.Log($"hit {hit.collider.gameObject.name} at {transform.forward.z * weaponRange}m away");
+            }
+        }
+        yield return new WaitForSeconds(weaponCoolDown);
+        firedWeapon = false;
     }
 }
