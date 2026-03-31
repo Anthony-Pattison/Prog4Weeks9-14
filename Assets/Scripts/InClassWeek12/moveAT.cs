4af44fb839a3cbbb9470dec78cf7736ad4694f27
pattisoa <pattisoa@OA-S141-23>
2026-03-31 18:17:50 -0400

    work from in class week 12

---
+++ Assets/Scripts/InClassWeek12/moveAT.cs
@@ -0,0 +1,33 @@
+using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+using UnityEngine;
+using UnityEngine.InputSystem.Android;
+
+namespace NodeCanvas.Tasks.Actions {
+
+	public class moveAT : ActionTask {
+		
+		public BBParameter<Vector3> moveDirectionBBP;
+		public BBParameter<float> moveSpeedBBP;
+		public BBParameter<float> turnSpeedBBP;
+
+
+		protected override void OnUpdate() {
+
+			Vector3 planer = new(moveDirectionBBP.value.x, 0, moveDirectionBBP.value.z);
+
+			// get the look rotation for the desired location
+			Quaternion desiredRotation = Quaternion.LookRotation(planer);
+			Quaternion currentRotation = agent.transform.rotation;
+
+			// point forward vector towards the destination
+            agent.transform.rotation = Quaternion.RotateTowards(currentRotation, desiredRotation, turnSpeedBBP.value * Time.deltaTime);
+
+			// apply the transform
+			agent.transform.position += moveSpeedBBP.value * Time.deltaTime * agent.transform.forward;
+
+			moveDirectionBBP.value = Vector3.zero;
+		}
+
+	}
+}
